6d5cac45eafb74870c3040b8df9a005a50c9760f
victor7262 <vmachadot@gmail.com>
2022-03-16 20:16:54 -0300

    adicionadas perguntas colapsaveis

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,10 +6,10 @@ let listaDeQuizzesGlobal = [],
 iniciarSite();
 
 function iniciarSite() {
-	// exibirTela("loading");
-	//buscarTodosQuizzes();
+	exibirTela("loading");
+	buscarTodosQuizzes();
 
-	CriarQuizz();
+	//CriarQuizz();
 }
 
 function buscarTodosQuizzes() {
